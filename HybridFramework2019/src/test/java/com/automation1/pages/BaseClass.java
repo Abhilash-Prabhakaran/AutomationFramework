dbbec234307f0ccb55b9eb296890215545831b92
Lenovo <Lenovo@LENRADRYZ-1>
2023-02-14 17:08:02 +0530

    Added few new code lines.
    Signed-off-by: Lenovo <Lenovo@LENRADRYZ-1>

--- HybridFramework2019/src/test/java/com/automation1/pages/BaseClass.java
+++ HybridFramework2019/src/test/java/com/automation1/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.automation1.utility.BrowserFactory;
 import com.automation1.utility.ConfigDataProvider;
@@ -44,12 +45,15 @@ public class BaseClass
 		Reporter.log("Settings done- Test can be started", true);
 	}
 	
+	
+	@Parameters({"browser","urlToBeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String Browser,String qaURL)
+	
 	{
 		Reporter.log("Trying to start Browser and Getting application ready", true);
 		
-		driver=BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+		driver = BrowserFactory.startApplication(driver, Browser, qaURL);
 		
 		Reporter.log("Browser and application is up and running", true);
 	}
